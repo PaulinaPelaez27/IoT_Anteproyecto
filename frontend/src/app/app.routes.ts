c40faec80d173081918b39d36ebd1e3f50e0792d
JorgeReyes2023 <126907208+JorgeReyes2023@users.noreply.github.com>
2025-12-02 19:04:13 -0500

    Se creo la pagina de listar empresas, y se ajustaron algunos colores y estilos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { ListaEmpresas } from './pages/lista-empresas/lista-empresas';
 
-export const routes: Routes = [];
+export const routes: Routes = [ { path: 'empresas', component: ListaEmpresas } ];
